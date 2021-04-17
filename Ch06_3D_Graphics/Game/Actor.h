2f4df3a6c80b6e4a18b5709b995970d4addc3bbc
Seong Beom Choi <s004402@gmail.com>
2021-04-17 23:07:24 +0900

    Ch06_3D_Graphics - init

---
+++ Ch06_3D_Graphics/Game/Actor.h
@@ -0,0 +1,67 @@
+#pragma once
+#include "Math.h"
+#include <vector>   
+class Actor
+{
+public:
+    // 액터의 상태를 추적하는데 사용된다.
+    enum State
+    {
+        EActive,
+        EPaused,
+        EDead
+    };
+    // 의존성 주입(dependency injection). actor 생성자가 Game 클래스의 포인터를 받는다.
+    // Actor는 다른 Actor를 생성하거나 Game 함수에 접근하기 위햏 이 포인터를 사용할 수 있다.
+    Actor(class Game* game);
+    virtual ~Actor();
+
+    // Game에서 호출하는 Update 함수 (가상 함수 아님 주의)
+    void Update(float deltaTime);
+    // 액터에 부착된 모든 컴포넌트를 업데이트 (가상 함수 아님 주의)
+    void UpdateComponents(float deltaTime);
+    // 특정 액터에 특화된 업데이트 코드 (오버라이딩 가능)
+    virtual void UpdateActor(float deltaTime);
+
+    // Getters/setters
+    const Vector2& GetPosition() const { return mPosition; }
+    void SetPosition(const Vector2& pos) { mPosition = pos; mRecomputeWorldTransform = true; }
+    float GetScale() const { return mScale; }
+    void SetScale(float scale) { mScale = scale; mRecomputeWorldTransform = true; }
+    float GetRotation() const { return mRotation; }
+    void SetRotation(float rotation) { mRotation = rotation; mRecomputeWorldTransform = true; }
+
+    void ComputeWorldTransform();
+    const Matrix4& GetWorldTransform() const { return mWorldTransform; }
+
+    State GetState() const { return mState; }
+    void SetState(State state) { mState = state; }
+
+    Vector2 GetForward() const { return Vector2(Math::Cos(mRotation), Math::Sin(mRotation)); }
+
+    class Game* GetGame() { return mGame; }
+
+    // 컴포넌트 추가/제거
+    void AddComponent(class Component* component);
+    void RemoveComponent(class Component* component);
+
+	void ProcessInput(const uint8_t* keyState);
+	virtual void ActorInput(const uint8_t* keyState) {}
+
+private:
+    // 액터의 상태
+    State mState;
+
+    // 변환
+    Matrix4 mWorldTransform;
+    Vector2 mPosition;  // 액터의 중심점
+    float mScale;       // 액터의 배율 (100%의 경우 1.0f)
+    float mRotation;    // 회전 각도 (라디안)
+    bool mRecomputeWorldTransform;
+
+    // 이 액터가 보유한 컴포넌트들
+    std::vector<class Component*> mComponents;
+
+    // 액터가 속한 Game 객체의 주소
+    class Game* mGame;
+};
---
+++ Ch06_3D_Graphics/Game/Game.h
@@ -0,0 +1,72 @@
+#pragma once
+#include "SDL/SDL.h"
+#include "Texture.h"
+#include <unordered_map>
+#include <string>
+#include <vector>
+
+class Game
+{
+public:
+    Game();
+    bool Initialize();  // 게임 초기화
+    void RunLoop();     // 게임이 끝나기 전까지 게임 루프 실행
+    void Shutdown();    // 게임 종료
+
+    // 엑터 추가/삭제
+    void AddActor(class Actor* actor);   
+    void RemoveActor(class Actor* actor);
+
+    // sprite 추가/삭제
+    void AddSprite(class SpriteComponent* sprite);  
+    void RemoveSprite(class SpriteComponent* sprite);
+
+    Texture* GetTexture(const std::string& fileName);
+
+	// Game-specific (add/remove asteroid)
+	void AddAsteroid(class Asteroid* ast);
+	void RemoveAsteroid(class Asteroid* ast);
+	std::vector<class Asteroid*>& GetAsteroids() { return mAsteroids; }
+
+private:
+    // 게임 루프를 위한 헬퍼 함수
+    void ProcessInput();
+    void UpdateGame();
+    void GenerateOutput();
+    void CreateSpriteVerts();   // 스프라이트를 그리기 위한 버텍스 생성
+    void LoadData();
+    void UnloadData();
+    bool LoadShader();
+
+    // 텍스처 load를 위한 Map
+    std::unordered_map<std::string, Texture*> mTextures;
+
+    // 활성화된 액터
+    std::vector<class Actor*> mActors;
+    // 대기 중인 액터, mActors를 반복하는 동안 새 액터를 생성하는 경우를 위해서 대기 액터를 위한 벡터 사용.
+    std::vector<class Actor*> mPendingActors;
+
+    //sprite를 저장하기 위한 벡터
+    std::vector<class SpriteComponent*> mSprites;
+
+    SDL_Window* mWindow;    // SDL이 생성한 window
+    bool mIsRunning;        // 게임이 계속 실행돼야 하는지를 판단.
+    bool mUpdatingActors;
+
+    SDL_Renderer* mRenderer;
+
+    //OpenGL 변수
+    SDL_GLContext mContext; // context는 OpenGL에서 하나의 세계를 뜻한다. (한 프로그램에 복수개의 context도 가능하다.)
+    // Sprite vertex array
+    class VertexArray* mSpriteVerts;
+    // Sprite를 그리기 위한 셰이더
+    class Shader* mSpriteShader;
+
+    // 델타 타임 구하기 위한 변수
+    Uint32 mTicksCount;
+
+	// Game-specific
+	class Ship* mShip; // Player's ship
+	std::vector<class Asteroid*> mAsteroids;
+
+};
