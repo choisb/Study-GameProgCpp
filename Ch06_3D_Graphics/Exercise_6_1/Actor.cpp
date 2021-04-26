eaa73b9496063500155a1f42d0cac1a5ce568439
Seong Beom Choi <s004402@gmail.com>
2021-04-26 23:00:13 +0900

    Ch06_3D_Graphics_Ex6_1 - init

---
+++ Ch06_3D_Graphics/Exercise_6_1/Actor.cpp
@@ -0,0 +1,93 @@
+#include "Actor.h"
+#include "Component.h"
+#include "Game.h"
+
+Actor::Actor(Game* game)
+    :mState(EActive)
+    ,mPosition(Vector3::Zero)
+    ,mScale(1.0f)
+    ,mRotation(Quaternion::Identity)
+    ,mGame(game)
+    ,mRecomputeWorldTransform(true)
+{
+    mGame->AddActor(this);
+}
+Actor::~Actor()
+{
+    mGame->RemoveActor(this);
+	while (!mComponents.empty())
+	{
+		delete mComponents.back();
+	}
+}
+// Game이 호출하는 코드
+void Actor::Update(float deltaTime)
+{
+    if (mState == EActive)
+    {
+        ComputeWorldTransform();
+
+        UpdateComponents(deltaTime);
+        UpdateActor(deltaTime);
+
+        ComputeWorldTransform();
+    }
+}
+// Components들 업데이트 호출
+void Actor::UpdateComponents(float deltaTime)
+{
+    for (auto comp : mComponents)
+    {
+        comp->Update(deltaTime);
+    }
+}
+void Actor::UpdateActor(float deltaTime)
+{
+    // actor의 업데이트 코드
+}
+void Actor::AddComponent(Component* component)
+{
+    auto iter = mComponents.begin();
+    for (; iter != mComponents.end(); iter++)
+    {
+        if ((*iter)->GetUpdateOrder() > component->GetUpdateOrder())
+            break;
+    }
+    mComponents.insert(iter, component);
+}
+void Actor::RemoveComponent(Component* component)
+{
+    auto iter = std::find(mComponents.begin(), mComponents.end(), component);
+    if (iter != mComponents.end())
+    {
+        mComponents.erase(iter);
+    }
+}
+void Actor::ProcessInput(const uint8_t* keyState)
+{
+	if (mState == EActive)
+	{
+		for (auto comp : mComponents)
+		{
+			comp->ProcessInput(keyState);
+		}
+		ActorInput(keyState);
+	}
+}
+void Actor::ComputeWorldTransform()
+{
+    if (mRecomputeWorldTransform)
+    {
+        mRecomputeWorldTransform = false;
+        // 스케일, 회전, 이동 행렬 순으로 결합해서 세계 변환 행렬 구함
+        mWorldTransform = Matrix4::CreateScale(mScale);
+        mWorldTransform *= Matrix4::CreateFromQuaternion(mRotation);
+        mWorldTransform *= Matrix4::CreateTranslation(mPosition);
+        
+        // 컴포넌트에 세계 변환이 갱신됐다고 통지
+        for (auto comp : mComponents)
+        {
+            comp->OnUpdateWorldTransform();
+        }
+    }
+}
